465159ab80763ed0da006df608acedd9d5dff0ee
Younger <youngerliu@gmail.com>
2019-07-18 23:23:30 +0800

    fix waitforgreen logic before unlock

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -87,12 +87,11 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
     //std::cout << "Traffic Light is: " << _trafficLight.getCurrentPhase() << std::endl; 
     // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen to block the execution until the traffic light turns green.
-    //if(_trafficLight.getCurrentPhase() != TrafficLightPhase::green) {   
-    lck.unlock();
-
+    //if(_trafficLight.getCurrentPhase() != TrafficLightPhase::green) {
     if(_trafficLight.getCurrentPhase() == TrafficLightPhase::red) {
         _trafficLight.waitForGreen();
-    }
+    }   
+    lck.unlock();
 }
 
 void Intersection::vehicleHasLeft(std::shared_ptr<Vehicle> vehicle)
